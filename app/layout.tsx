9da85cf3fb314e8c26564db695c91150ee26913c
AlanGolphi <alangolphi@163.com>
2024-01-16 13:55:13 +0800

    feat: add vercel analytics and speed-insights pkgs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
+import {Analytics} from '@vercel/analytics/react'
+import {SpeedInsights} from '@vercel/speed-insights/next'
 
 export const metadata: Metadata = {
   title: "Wuds Run",
@@ -28,6 +30,8 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
