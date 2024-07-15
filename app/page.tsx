d56ecc7120cc8a0aa747cfa989b97d91ef1b1e15
AlanGolphi <alangolphi@163.com>
2024-07-15 16:14:30 +0800

    remove buggy next-auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { SignIn } from "@/components/next-auth/sign-in"
-
 export default function Home() {
-  return <SignIn />
+  return null
 }
--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,3 @@
-export { auth as middleware } from "@/auth"
-
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 }
